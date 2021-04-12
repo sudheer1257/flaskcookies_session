035e0701e4d1fd4007b588311add0e18b31b766f
saichinmay2000 <saichinmayt@gmail.com>
2021-04-12 14:30:57 +0530

    Initial commit

---
+++ cookies1.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/cookie')
+def cookie():
+    res = make_response("<h1>cookie is set</h1>")
+    res.set_cookie('foo', 'bar')
+    return res
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
